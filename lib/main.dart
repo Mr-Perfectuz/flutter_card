6aaed5832c7aa7c9989af2c9c234d317749d8b07
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-02 14:05:44 +0500

    main widget has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -15,32 +15,39 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         body: SafeArea(
             child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Container(
               padding: EdgeInsets.all(30.0),
-              color: Colors.yellow,
-              width: 200.0,
+              color: Colors.red,
+              width: 100.0,
+              height: 750.0,
+              // child: Text(
+              //   'Pohang University',
+              //   textDirection: TextDirection.rtl,
+              //   style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
+              // ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+              width: 100.0,
               height: 100.0,
-              child: Text(
-                'Shodiyev Docker',
-                textDirection: TextDirection.rtl,
-                style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
-              ),
+              color: Colors.yellow,
             ),
             Container(
               padding: EdgeInsets.all(30.0),
-              color: Colors.deepPurple.shade50,
-              width: 191.0,
-              height: 100.0,
-              child: Text(
-                'Sogang University',
-                textDirection: TextDirection.rtl,
-                style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
-              ),
+              color: Colors.blue,
+              width: 100.0,
+              height: 750.0,
+              // child: Text(
+              //   'Sogang University',
+              //   textDirection: TextDirection.rtl,
+              //   style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
+              // ),
             ),
           ],
         )),
