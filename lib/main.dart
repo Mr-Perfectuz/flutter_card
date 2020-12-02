5cca252f658205f4541fc99c65da27a2d3bd524e
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-02 11:03:26 +0500

    main container has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,77 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.redAccent,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Container(
               padding: EdgeInsets.all(30.0),
               color: Colors.red[100],
+              width: 100.0,
+              height: 100.0,
+              child: Text(
+                'Hello',
+                textDirection: TextDirection.rtl,
+                style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.blue[100],
+              width: 100.0,
+              height: 100.0,
+              child: Text(
+                'Hello',
+                textDirection: TextDirection.rtl,
+                style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.yellow,
+              width: 100.0,
+              height: 100.0,
+              child: Text(
+                'Hello',
+                textDirection: TextDirection.rtl,
+                style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.black,
+              width: 100.0,
+              height: 100.0,
+              child: Text(
+                'Hello',
+                textDirection: TextDirection.rtl,
+                style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.deepPurple.shade50,
+              width: 100.0,
+              height: 100.0,
+              child: Text(
+                'Hello',
+                textDirection: TextDirection.rtl,
+                style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.brown,
+              width: 100.0,
+              height: 100.0,
+              child: Text(
+                'Hello',
+                textDirection: TextDirection.rtl,
+                style: TextStyle(fontSize: 15.0, color: Colors.deepPurple),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.purpleAccent,
               width: 100.0,
               height: 100.0,
               child: Text(
