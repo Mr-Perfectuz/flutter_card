2ae566dc17c2e6197a2b9809b7014297c127b5bb
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-03 15:55:58 +0500

    main widget has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -15,20 +15,75 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: Column(
           children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.blue,
+              backgroundImage: AssetImage('image/person.jpg'),
+              radius: 50.0,
+            ),
+            Text(
+              'Angela Yeu',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'FLUTTER DEVELOPMENT',
+              style: TextStyle(fontSize: 20.0, color: Colors.white),
+            ),
             Container(
-              margin: EdgeInsets.fromLTRB(150.0, 200.0, 0.0, 10.0),
-              child: CircleAvatar(
-                backgroundColor: Colors.blue,
-                backgroundImage: NetworkImage(
-                    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bigstockphoto.com%2F&psig=AOvVaw2a78_IY-8ED4kc23ZhrDtR&ust=1607068032480000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCTj5upse0CFQAAAAAdAAAAABAD'),
-                radius: 50.0,
+              margin: EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 10.0,
+              ),
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.call,
+                    size: 30.0,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  Text(
+                    '+44 1234 5678 901',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.teal,
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(100.0, 5.0, 0.0, 10.0),
-              child: Text(
-                'Alexandra',
-                style: TextStyle(fontFamily: 'Pacifico'),
+              margin: EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 10.0,
+              ),
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    size: 30.0,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  Text(
+                    'angela@gmail.com',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.teal,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
